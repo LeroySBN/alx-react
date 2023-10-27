6c57ec88cf9331b91771ff42b1443a82c38e8460
leroysb <leroybuliro@gmail.com>
2023-10-27 13:15:26 +0300

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -10,8 +10,11 @@ $('body').append('<p>Copyright - Holberton School</p>');
 let count = 0;
 
 const updateCounter = ()=> {
-  count+=1;
+  count++;
   $('#count').text(`${count} clicks on the button`);
 }
 
-$('button').on('click', _.debounce(updateCounter));
+$('button').on('click', _.debounce(updateCounter, 1000, {
+  'leading': true,
+  'trailing': false
+}));
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
