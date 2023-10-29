11ee5404152a2274b642fac3f7d3ba224899cf1f
leroysb <leroybuliro@gmail.com>
2023-10-29 16:35:36 +0300

    Create basic tests with four tests

---
+++ 0x01-react_intro/task_3/dashboards/src/utils.js
@@ -0,0 +1,16 @@
+const getFullYear = () => new Date().getFullYear();
+
+const getFooterCopy = (isIndex) => {
+  if (isIndex) return 'Holberton School';
+  return 'Holberton School main dashboard';
+}
+
+const getLatestNotification = () => {
+  return ('<strong>Urgent requirement</strong> - complete by EOD');
+}
+
+module.exports = {
+  getFullYear,
+  getFooterCopy,
+  getLatestNotification,
+};
