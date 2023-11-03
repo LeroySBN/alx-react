952a71e7ae31f551982c0402d7e4bd7872456fc0
leroysb <leroybuliro@gmail.com>
2023-11-03 10:22:25 +0300

    Shapes, Loops, and Keys

---
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
+
+export default function App({ isLoggedIn }) {
+  return (
+    <Fragment>
+      <Notifications listNotifications={listNotifications} />
+      <div className="App">
+        <Header />
+        {isLoggedIn ? ( <CourseList listCourses={listCourses} /> ) : ( <Login /> ) }
+        <Footer />
+      </div>
+    </Fragment>
+  );
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+}
