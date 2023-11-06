5300e3d5658b9c88cf90705c12e21a8f2607179c
leroysb <leroybuliro@gmail.com>
2023-11-06 12:01:36 +0300

    Lifecycles

--- 0x03-React_component/task_1/dashboard/src/index.js
+++ 0x03-React_component/task_1/dashboard/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+  root
+  );
+  
+  // const root = ReactDOM.createRoot(document.getElementById('root'));
+  // root.render(
+  //   <React.StrictMode>
+  //     <App />
+  //   </React.StrictMode>
+  // );
+  
