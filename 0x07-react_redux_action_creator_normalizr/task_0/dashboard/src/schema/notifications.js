f54e027810b86d30e83823f70958b90e72c56486
leroysb <leroybuliro@gmail.com>
2024-01-02 08:46:05 +0300

    read data from a json

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,18 +1,8 @@
 import notificationsData from '../../../../notifications.json'
 
 export default function getAllNotificationsByUser(userId) {
-  // const userNotifications = [];
-  
-  // for (const notification of notificationsData) {
-  //   if (notification.author.id === userId) {
-  //     userNotifications.push(notification.context);
-  //   }
-  // };
-  
-  // console.log(userNotifications);
-  // return userNotifications;
   const userNotifications = notificationsData.filter(notification => notification.author.id === userId);
+  console.log(userNotifications);
   const contexts = userNotifications.map(notification => notification.context);
   return contexts;
 }
-
