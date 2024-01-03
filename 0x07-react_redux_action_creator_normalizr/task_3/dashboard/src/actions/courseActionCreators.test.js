04f37dd79680bf1a692172662e1f24d72404e087
leroysb <leroybuliro@gmail.com>
2024-01-03 12:25:30 +0300

    Create actions for the course list

---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -0,0 +1,17 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import { selectCourse, unSelectCourse } from "./courseActionCreators";
+
+describe("Test selectCourse action", () => {
+    it("selectCourse returns correct object", () => {
+        const result = selectCourse(1);
+        expect(result).toEqual({ type: "SELECT_COURSE", index: 1 });
+    });
+});
+
+describe("Test unSelectCourse action", () => {
+    it("unSelectCourse returns correct object", () => {
+        const result = unSelectCourse(1);
+        expect(result).toEqual({ type: "UNSELECT_COURSE", index: 1 });
+    });
+});
